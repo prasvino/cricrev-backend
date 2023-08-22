78372b512cf689ae01d6c0b6f505bd1f8a8c88ed
Prasanna Muthukumaran <prasvino10@gmail.com>
2023-08-22 10:04:13 +0530

    java version set to 11

--- src/main/java/com/cricapp/cricrev/CricRevApplication.java
+++ src/main/java/com/cricapp/cricrev/CricRevApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CricRevApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("WELCOME TO CRIC REV!!!!");
 		SpringApplication.run(CricRevApplication.class, args);
+
 	}
 
 }
