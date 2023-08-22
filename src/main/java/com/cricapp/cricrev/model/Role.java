069d738a66f0411b55fe97ae04a53dcc67d4e506
Prasanna Muthukumaran <prasvino10@gmail.com>
2023-08-22 17:39:03 +0530

    endpoint name modified for submit

---
+++ src/main/java/com/cricapp/cricrev/model/Role.java
@@ -0,0 +1,35 @@
+package com.cricapp.cricrev.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import jakarta.persistence.*;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(length = 60)
+    private String name;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/com/cricapp/cricrev/web/controller/CricRevController.java
+++ src/main/java/com/cricapp/cricrev/web/controller/CricRevController.java
@@ -19,5 +19,5 @@ public class CricRevController {
 
 /** @PostMapping("/register")
 
-    @PostMapping("insert") **/
+    @PostMapping("/submit") **/
 }
