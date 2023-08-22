f2d707a6a417ddd17a5a2fe8f581e1b31e0e74fb
Prasanna Muthukumaran <prasvino10@gmail.com>
2023-08-22 09:57:04 +0530

    Backend service generated

---
+++ src/main/java/com/cricapp/cricrev/CricRevApplication.java
@@ -0,0 +1,13 @@
+package com.cricapp.cricrev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricRevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricRevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cricapp/cricrev/CricRevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cricapp.cricrev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricRevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
